e263271817d366f33785614c573269fcf424d992
seufernandez <fernandez.g1337@gmail.com>
2021-03-25 16:00:06 -0900

    js embeded

--- scripts.js
+++ scripts.js
@@ -1,14 +1,21 @@
 const modalOverlay = document.querySelector(".modal-overlay")
 const cards = document.querySelectorAll("#play")//pega todos com a classe card
 
-
+var video
 for (let card of cards) {
     card.addEventListener("click", function(){
-        modalOverlay.classList.add("active")
-    })
+        const videoId = card.getAttribute("title");//pegando id
+        modalOverlay.classList.add("active");
+        modalOverlay.querySelector("iframe").src =`https://www.youtube.com/embed/${videoId}`
+        console.log(videoId);
+    });
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
+
 
 });
+
+
